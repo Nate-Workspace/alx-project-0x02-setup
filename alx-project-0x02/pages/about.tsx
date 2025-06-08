e54b1ce2927bedeec8e9d4fab78f70fe8fe89dc8
Nate-Workspace <nathanisrael.jobs@gmail.com>
2025-06-08 17:11:23 +0300

    Button component - Task 5

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,3 +1,4 @@
+import Button from '@/components/common/Button';
 import Header from '@/components/layout/Header';
 import React from 'react';
 
@@ -7,6 +8,11 @@ const About = () => {
     <Header/>
     <div className="p-8">
       <h1 className="text-2xl font-bold">Welcome to the About Page</h1>
+      <div className="space-x-4">
+        <Button label="Small" size="small" shape="rounded-sm" />
+        <Button label="Medium" size="medium" shape="rounded-md" />
+        <Button label="Large" size="large" shape="rounded-full" />
+      </div>
     </div>
     </div>
   );
